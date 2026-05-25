3b0b3753db37648006da056f1a249b8c1ff03237
Kentox3 <rony.merta1@gmail.com>
2026-05-25 23:04:33 +0200

    Aktualisieren von config.js

--- js/config.js
+++ js/config.js
@@ -1,36 +1,31 @@
 export const API_URL =
-  "https://script.google.com/macros/s/AKfycbzJnFU-3C8U0EpnnbvohU57t1cWQebdQVQCx76yBq_3bOtS_5POeba4uZkTaMcp93h7/exec";
+  "https://script.google.com/macros/s/AKfycbz1dpJirxQil5jMHxD-Tp-ee7Bpi6-vMsetNING8R5NsmCizuA9szlhev5okPP5oEDh/exec";
 
 export const API_KEY = "272520";
 export const parentPin = "272520";
 export const maxPoints = 100;
 
+export const SHEETS = {
+  kids: "1",
+  rewards: "2",
+  streaks: "3",
+  purchases: "4"
+};
+
 export const kidsConfig = {
-  Luna: {
-    row: 2,
-    pin: "271",
-    className: "luna",
-    contributionCol: "I"
-  },
-  Milo: {
-    row: 3,
-    pin: "254",
-    className: "milo",
-    contributionCol: "J"
-  },
-  Finn: {
-    row: 4,
-    pin: "207",
-    className: "finn",
-    contributionCol: "K"
-  }
+  Luna: { row: 2, pin: "271", className: "luna", contributionCol: "I" },
+  Milo: { row: 3, pin: "254", className: "milo", contributionCol: "J" },
+  Finn: { row: 4, pin: "207", className: "finn", contributionCol: "K" }
 };
 
 export const firstLootColumn = 4;
 export const lootSlots = 20;
 
-export const rewardsStartRow = 11;
-export const rewardsEndRow = 40;
+export const rewardsStartRow = 2;
+export const rewardsEndRow = 200;
 
-export const streaksStartRow = 11;
-export const streaksEndRow = 100;
+export const streaksStartRow = 2;
+export const streaksEndRow = 500;
+
+export const purchasesStartRow = 2;
+export const purchasesEndRow = 1000;
