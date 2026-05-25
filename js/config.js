1fff7390365c65df102450aa422d6b89b3a703bc
Kentox3 <rony.merta1@gmail.com>
2026-05-25 14:09:03 +0200

    Aktualisieren von config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,48 @@
-// Configuration settings
+export const API_URL =
+  "https://script.google.com/macros/s/AKfycbzJnFU-3C8U0EpnnbvohU57t1cWQebdQVQCx76yBq_3bOtS_5POeba4uZkTaMcp93h7/exec";
 
-const config = {
-  apiUrl: 'https://api.example.com',
-  version: '1.0.0'
+export const API_KEY =
+  "272520";
+
+export const parentPin =
+  "272520";
+
+export const maxPoints =
+  100;
+
+export const kidsConfig = {
+
+  Luna: {
+    row: 2,
+    pin: "271",
+    className: "luna",
+    contributionCol: "I"
+  },
+
+  Milo: {
+    row: 3,
+    pin: "254",
+    className: "milo",
+    contributionCol: "J"
+  },
+
+  Finn: {
+    row: 4,
+    pin: "207",
+    className: "finn",
+    contributionCol: "K"
+  }
+
 };
+
+export const firstLootColumn =
+  4;
+
+export const lootSlots =
+  20;
+
+export const rewardsStartRow =
+  11;
+
+export const rewardsEndRow =
+  40;
