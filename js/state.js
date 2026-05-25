031eaeabb0e604544f49d61a2a17f1f80dbcd73b
Kentox3 <rony.merta1@gmail.com>
2026-05-25 23:10:55 +0200

    Aktualisieren von state.js

--- js/state.js
+++ js/state.js
@@ -1,9 +1,13 @@
 export const state = {
   unlockedChild: null,
+
   kidsData: [],
   rewardsData: [],
   streaksData: [],
+  purchasesData: [],
+
   selectedAdminChild: null,
+
   slideTick: 0,
   rewardCooldown: false,
   isSaving: false
